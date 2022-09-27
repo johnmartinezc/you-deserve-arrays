bf472ed5ebca2489d898858d27d00bd09e60af7a
johnmartinezc <john.martinez@codeimmersives.com>
2022-09-27 10:40:52 -0400

    -a

--- isLongList.js
+++ isLongList.js
@@ -2,7 +2,7 @@ const prompt = require('prompt-sync')({ sigint: true });
 
 let array = JSON.parse(prompt("Enter an array: "))
 
-if (array.length <= 10) {
+if (array.length >= 10) {
 
     console.log(true)
 } else {
